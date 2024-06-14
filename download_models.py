1bf51da06c3bdeb6b03f5b9b10adc261bd662154
Justin Lin <justinwlin@gmail.com>
2024-06-14 13:10:16 -0400

    Moving to my own hf links

--- download_models.py
+++ download_models.py
@@ -59,11 +59,10 @@ def get_remote_file_size(url):
 folders_and_files = {
     os.path.join('models'): [
         ('https://huggingface.co/laion/CLIP-ViT-bigG-14-laion2B-39B-b160k/resolve/main/open_clip_pytorch_model.bin', None),
-        ('https://huggingface.co/ashleykleynhans/SUPIR/resolve/main/SUPIR-v0F.ckpt', 'v0F.ckpt'),
-        ('https://huggingface.co/ashleykleynhans/SUPIR/resolve/main/SUPIR-v0Q.ckpt', 'v0Q.ckpt')
+        ('https://huggingface.co/justinwlinx/supir1/resolve/main/SUPIR-v0F.ckpt', 'v0F.ckpt'),
+        ('https://huggingface.co/justinwlinx/supir1/resolve/main/SUPIR-v0Q.ckpt', 'v0Q.ckpt')
     ]
 }
-
 
 if __name__ == '__main__':
     for folder, files in folders_and_files.items():
